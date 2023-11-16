398e6ef9e365ecdbf4c5b8a00ca6d6c32928b440
Brandon Perfetti <brandon@brandonperfetti.com>
2023-11-16 14:14:45 -0800

    Add jwt authentication.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,27 +1,31 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-  private readonly DataContext _context;
-  public UsersController(DataContext context)
+  [Authorize]
+  public class UsersController : BaseApiController
   {
-    _context = context;
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+      _context = context;
+    }
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      var users = await _context.Users.ToListAsync();
+      return users;
+    }
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
   }
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-  {
-    var users = await _context.Users.ToListAsync();
-    return users;
-  }
-  [HttpGet("{id}")] // /api/users/2
-  public async Task<ActionResult<AppUser>> GetUser(int id)
-  {
-    return await _context.Users.FindAsync(id);
-  }
+
 }
---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+
+  public interface ITokenService
+  {
+    string CreateToken(AppUser user);
+
+  }
+}
+
