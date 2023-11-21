0eaea363328523ab125c29dbbdcd4fa3c7599a2e
Brandon Perfetti <brandon@brandonperfetti.com>
2023-11-21 13:11:16 -0800

    Implement the like user functionality.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -9,11 +9,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(
-    public accountService: AccountService,
-    private router: Router
-  ) // private toastr: ToastrService
-  {}
+  constructor(public accountService: AccountService, private router: Router) {}
   ngOnInit(): void {}
 
   login() {
